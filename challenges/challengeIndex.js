cacef6453a72e981d11f8f3357ea27f92db28ce5
s-knite <158498427+s-knite@users.noreply.github.com>
2025-10-03 12:19:52 +0100

    Create challengeIndex.js

---
+++ challenges/challengeIndex.js
@@ -0,0 +1,14 @@
+// challenges/challengeIndex.js
+
+export const challenges = [
+    {
+        id: 'variables-warm-up', 
+        title: 'Variables - Warm Up',
+        description: 'An introduction to declaring variables with correct type hints.'
+    },
+    {
+        id: 'functions-intro',
+        title: 'Introduction to Functions',
+        description: 'Learn how to define and call basic Python functions.'
+    }
+];
