5faee882589d08baa3c291b44a6713b5733af737
s-knite <158498427+s-knite@users.noreply.github.com>
2025-11-11 11:49:12 +0000

    Add default challenges for demonstration purposes

---
+++ challenges/default/index.js
@@ -0,0 +1,9 @@
+//default challenges for demonstration purposes
+
+export const challenges = [
+    {
+        id: 'demo', 
+        title: 'Demo',
+        description: 'This challenge is used to demonstrate the platform.\nThis can be skipped.'
+    },
+];
