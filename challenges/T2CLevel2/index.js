83df358cda9f96816cac56a2c81a7e2473d8475b
s-knite <158498427+s-knite@users.noreply.github.com>
2025-11-12 08:50:01 +0000

    Add Time2Code level 2 challenges data

---
+++ challenges/T2CLevel2/index.js
@@ -0,0 +1,10 @@
+//Code checking for Time2Code level 2
+
+export const challenges = [
+    {
+        id: 't2c-02-04', 
+        title: 'Time2Code 02-04',
+        description: 'States of Water ⭐'
+    },
+   
+];
