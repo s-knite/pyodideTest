4016712547fd7a87431398443441365bfdbb2690
s-knite <158498427+s-knite@users.noreply.github.com>
2025-11-18 13:05:05 +0000

    Fix escape characters in dispense function tests

--- challenges/T2CLevel3/t2c-03-06.js
+++ challenges/T2CLevel3/t2c-03-06.js
@@ -13,12 +13,12 @@ tester = Tester()
 try:
 	tester.check_function_exists("dispense")
 	tester.check_function_parameters("dispense", 1)
-	tester.full_check("dispense", args=["50"], e_out="W50\nD20\nD20\nD10", quiet = False)
-	tester.full_check("dispense", args=["75"], e_out="W75\nD20\nD20\nD20\nD10\nD5", quiet = False)
+	tester.full_check("dispense", args=["50"], e_out="W50\\nD20\\nD20\\nD10", quiet = False)
+	tester.full_check("dispense", args=["75"], e_out="W75\\nD20\\nD20\\nD20\\nD10\\nD5", quiet = False)
 	tester.full_check("dispense", args=["0"], e_out="W0", quiet = False)
-	tester.full_check("dispense", args=["90"], e_out="W90\nD20\nD20\nD20\nD20\nD10", quiet = False)
-	tester.full_check("dispense", args=["200"], e_out="W200\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20", quiet = True)
-	tester.full_check("dispense", args=["315"], e_out=""W315\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD20\nD10\nD5"", quiet = True)
+	tester.full_check("dispense", args=["90"], e_out="W90\\nD20\\nD20\\nD20\\nD20\\nD10", quiet = False)
+	tester.full_check("dispense", args=["200"], e_out="W200\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20", quiet = True)
+	tester.full_check("dispense", args=["315"], e_out=""W315\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD20\\nD10\\nD5"", quiet = True)
 
 
 
