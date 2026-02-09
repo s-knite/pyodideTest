aaff3a8d774f30accfd91c5e85ad657a476d5c21
s-knite <158498427+s-knite@users.noreply.github.com>
2026-02-09 14:13:55 +0000

    Add Yr9 assessment challenge to challengeIndex.js

--- challenges/Yr9Assessment/challengeIndex.js
+++ challenges/Yr9Assessment/challengeIndex.js
@@ -1,1 +1,9 @@
+// challenges/challengeIndex.js
 
+export const challenges = [
+    {
+        id: 'yr9-assess-01', 
+        title: 'yr9-assess-01 ',
+        description: 'Variables ⭐'
+    },
+];
