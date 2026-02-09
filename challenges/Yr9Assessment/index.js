5e0a0d70935bc854d09775a34d741c4d16626642
s-knite <158498427+s-knite@users.noreply.github.com>
2026-02-09 14:53:40 +0000

    Add new assessments to Yr9Assessment

--- challenges/Yr9Assessment/index.js
+++ challenges/Yr9Assessment/index.js
@@ -6,4 +6,39 @@ export const challenges = [
         title: '01 ⭐',
         description: 'Variables '
     },
+        {
+        id: 'yr9-assess-02', 
+        title: '02 ⭐',
+        description: 'Strings'
+    },
+        {
+        id: 'yr9-assess-03', 
+        title: '03 ⭐',
+        description: 'Function '
+    },
+        {
+        id: 'yr9-assess-04', 
+        title: '04 ⭐⭐',
+        description: 'Selection'
+    },
+            {
+        id: 'yr9-assess-05', 
+        title: '05 ⭐⭐',
+        description: 'Strings '
+    },
+        {
+        id: 'yr9-assess-06', 
+        title: '06 ⭐⭐',
+        description: 'Math'
+    },
+                {
+        id: 'yr9-assess-07', 
+        title: '07 ⭐⭐⭐',
+        description: 'Selection with Math '
+    },
+        {
+        id: 'yr9-assess-08', 
+        title: '08 ⭐⭐⭐',
+        description: 'Lists'
+    },
 ];
