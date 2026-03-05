fae13e83709b4e1c3cd6adc018aa985441d45a5b
s-knite <158498427+s-knite@users.noreply.github.com>
2026-03-05 11:36:24 +0000

    Add challenges to Time2Code Level 4

---
+++ challenges/T2CLevel4/index.js
@@ -0,0 +1,32 @@
+export const challenges = [
+    {
+        id: 't2c-04-04', 
+        title: 'Time2Code 04-04',
+        description: 'Dungeon Master ⭐'
+    },
+    {
+        id: 't2c-04-05', 
+        title: 'Time2Code 04-05',
+        description: 'Leap Year ⭐'
+    },
+	{
+        id: 't2c-04-06', 
+        title: 'Time2Code 04-06',
+        description: 'Guess the Number ⭐⭐'
+    },
+	{
+        id: 't2c-04-07', 
+        title: 'Time2Code 04-07',
+        description: 'Time Converter⭐⭐'
+    },
+	{
+        id: 't2c-04-08', 
+        title: 'Time2Code 04-08',
+        description: 'LCM ⭐⭐⭐'
+    },
+	{
+        id: 't2c-04-09', 
+        title: 'Time2Code 04-09',
+        description: 'Rock, Paper, Scissors ⭐⭐⭐'
+    },
+];
