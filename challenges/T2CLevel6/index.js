3b757788fabd6ada0e39853260c9a7ad7996cd4d
s-knite <158498427+s-knite@users.noreply.github.com>
2026-05-31 10:12:14 +0100

    Create index.js with Time2Code challenges
    Added a list of Time2Code challenges with IDs and descriptions.

---
+++ challenges/T2CLevel6/index.js
@@ -0,0 +1,32 @@
+export const challenges = [
+    {
+        id: 't2c-06-04', 
+        title: 'Time2Code 06-04',
+        description: 'Times tables ⭐'
+    },
+    {
+        id: 't2c-06-05', 
+        title: 'Time2Code 06-05',
+        description: 'Voting count ⭐'
+    },
+	{
+        id: 't2c-06-06', 
+        title: 'Time2Code 06-06',
+        description: 'Fizz buzz ⭐⭐'
+    },
+	{
+        id: 't2c-06-07', 
+        title: 'Time2Code 06-07',
+        description: 'Find and replace ⭐⭐'
+    },
+	{
+        id: 't2c-06-08', 
+        title: 'Time2Code 06-08',
+        description: 'Twelve days of Christmas ⭐⭐⭐'
+    },
+	{
+        id: 't2c-06-09', 
+        title: 'Time2Code 06-09',
+        description: 'Distribution of two dice ⭐⭐⭐'
+    },
+];
