e256130cb2f952c3703c78ca244c9168c9db1ed7
s-knite <158498427+s-knite@users.noreply.github.com>
2026-05-31 11:21:26 +0100

    Refactor tests to check for function and variable existence
    Updated testing logic to check for existence of 'get_station' and 'calculate_stops' functions and 'stations' variable before executing tests. Added skipped test messages for missing components.

--- challenges/T2CLevel5/t2c-05-09.js
+++ challenges/T2CLevel5/t2c-05-09.js
@@ -11,37 +11,50 @@ export const challenge = {
     challengeTests: `
 tester = Tester()
 try:
+    # --- Global Variables ---
+    tester.check_variable_exists("stations")
+
     # --- get_station ---
     tester.check_function_exists("get_station")
     # Assuming get_station takes 0 arguments and relies on input()
     tester.check_function_parameters("get_station", 0)
 
-    # First try
-    tester.full_check("get_station", inputs=["Brixton"], e_return="Brixton", quiet=False)
-    
-    # Third try
-    tester.full_check("get_station", inputs=["V", "Vaux", "Vauxhall"], e_return="Vauxhall", quiet=False)
+    if tester.confirm_function_exists("get_station") and tester.confirm_variable_exists("stations"):
+        # First try
+        tester.full_check("get_station", inputs=["Brixton"], e_return="Brixton", quiet=False)
+        
+        # Third try
+        tester.full_check("get_station", inputs=["V", "Vaux", "Vauxhall"], e_return="Vauxhall", quiet=False)
 
-    # Eventually
-    tester.full_check("get_station", inputs=["A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C", "Oxford Circus"], e_return="Oxford Circus", quiet=False)
-
+        # Eventually
+        tester.full_check("get_station", inputs=["A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C", "Oxford Circus"], e_return="Oxford Circus", quiet=False)
+    else:
+        tester.include_result("Testing function get_station with inputs ['Brixton']. (Skipped - missing function or 'stations' variable)", SKIPPED)
+        tester.include_result("Testing function get_station with inputs ['V', 'Vaux', 'Vauxhall']. (Skipped - missing function or 'stations' variable)", SKIPPED)
+        tester.include_result("Testing function get_station with inputs [... 'Oxford Circus']. (Skipped - missing function or 'stations' variable)", SKIPPED)
 
     # --- calculate_stops ---
     tester.check_function_exists("calculate_stops")
     # calculate_stops takes 1 argument (a list containing two stations)
     tester.check_function_parameters("calculate_stops", 1)
 
-    # Stockwell to Oxford Circus
-    tester.full_check("calculate_stops", args=[["Stockwell", "Oxford Circus"]], e_return=5, quiet=False)
-    
-    # Brixton to Walthamstow Central
-    tester.full_check("calculate_stops", args=[["Brixton", "Walthamstow Central"]], e_return=15, quiet=False)
+    if tester.confirm_function_exists("calculate_stops") and tester.confirm_variable_exists("stations"):
+        # Stockwell to Oxford Circus
+        tester.full_check("calculate_stops", args=[["Stockwell", "Oxford Circus"]], e_return=5, quiet=False)
+        
+        # Brixton to Walthamstow Central
+        tester.full_check("calculate_stops", args=[["Brixton", "Walthamstow Central"]], e_return=15, quiet=False)
 
-    # Oxford Circus to Stockwell
-    tester.full_check("calculate_stops", args=[["Oxford Circus", "Stockwell"]], e_return=5, quiet=False)
+        # Oxford Circus to Stockwell
+        tester.full_check("calculate_stops", args=[["Oxford Circus", "Stockwell"]], e_return=5, quiet=False)
 
-    # Seven Sisters to Victoria
-    tester.full_check("calculate_stops", args=[["Seven Sisters", "Victoria"]], e_return=8, quiet=False)
+        # Seven Sisters to Victoria
+        tester.full_check("calculate_stops", args=[["Seven Sisters", "Victoria"]], e_return=8, quiet=False)
+    else:
+        tester.include_result("Testing function calculate_stops using arguments [['Stockwell', 'Oxford Circus']]. (Skipped - missing function or 'stations' variable)", SKIPPED)
+        tester.include_result("Testing function calculate_stops using arguments [['Brixton', 'Walthamstow Central']]. (Skipped - missing function or 'stations' variable)", SKIPPED)
+        tester.include_result("Testing function calculate_stops using arguments [['Oxford Circus', 'Stockwell']]. (Skipped - missing function or 'stations' variable)", SKIPPED)
+        tester.include_result("Testing function calculate_stops using arguments [['Seven Sisters', 'Victoria']]. (Skipped - missing function or 'stations' variable)", SKIPPED)
 
 except Exception as e:
     tester.results.append({
