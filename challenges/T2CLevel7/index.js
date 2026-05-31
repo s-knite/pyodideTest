6c8f6027b26b3bd565038832b3ed0d4ea544163e
s-knite <158498427+s-knite@users.noreply.github.com>
2026-05-31 11:40:04 +0100

    Add Time2Code challenges to index.js
    Added multiple Time2Code challenges with IDs and descriptions.

---
+++ challenges/T2CLevel7/index.js
@@ -0,0 +1,32 @@
+export const challenges = [
+    {
+        id: 't2c-07-04', 
+        title: 'Time2Code 07-04',
+        description: 'Airline ticket ⭐'
+    },
+    {
+        id: 't2c-07-05', 
+        title: 'Time2Code 07-05',
+        description: 'Disemvowel ⭐'
+    },
+	{
+        id: 't2c-07-06', 
+        title: 'Time2Code 07-06',
+        description: 'Your move ⭐⭐'
+    },
+	{
+        id: 't2c-07-07', 
+        title: 'Time2Code 07-07',
+        description: 'Tweet ⭐⭐'
+    },
+	{
+        id: 't2c-07-08', 
+        title: 'Time2Code 07-08',
+        description: 'Valid address ⭐⭐⭐'
+    },
+	{
+        id: 't2c-07-09', 
+        title: 'Time2Code 07-09',
+        description: 'Palindrome ⭐⭐⭐'
+    },
+];
