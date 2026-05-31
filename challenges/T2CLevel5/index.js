7ee9d8284edcbe7b7d3f2914058199a68ce571e3
s-knite <158498427+s-knite@users.noreply.github.com>
2026-05-31 10:10:33 +0100

    Add challenges for Time2Code 05 series

---
+++ challenges/T2CLevel5/index.js
@@ -0,0 +1,27 @@
+export const challenges = [
+    {
+        id: 't2c-05-04', 
+        title: 'Time2Code 05-04',
+        description: 'Random name generator ⭐'
+    },
+    {
+        id: 't2c-05-05', 
+        title: 'Time2Code 05-05',
+        description: 'Cup draw ⭐'
+    },
+	{
+        id: 't2c-05-07', 
+        title: 'Time2Code 05-07',
+        description: 'Darts ⭐⭐'
+    },
+	{
+        id: 't2c-05-08', 
+        title: 'Time2Code 05-08',
+        description: 'Cut the deck ⭐⭐⭐'
+    },
+	{
+        id: 't2c-05-09', 
+        title: 'Time2Code 05-09',
+        description: 'London underground ⭐⭐⭐'
+    },
+];
